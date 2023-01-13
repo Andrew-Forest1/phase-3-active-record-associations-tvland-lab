c757ab6e171dcb9e10fd287cdbb788091da3bc82
Andrew <aforest41@yahoo.com>
2023-01-13 15:29:05 -0800

    commit tree 8fb91fc89e2d0b23c4f6e91b25f5b015bc380921

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters #16
+  has_many :shows, through: :characters
+  validates :first_name, presence: true
+
+  def full_name
+      self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    self.characters.map do |c|
+        "#{c.name} - #{c.show.name}"
+    end
+  end
 end
